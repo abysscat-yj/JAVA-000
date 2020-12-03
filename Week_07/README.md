368603a21e88fa01a107ee38460c3a806635e7ee
yuanjie <yuanjie05@baidu.com>
2020-12-03 16:38:23 +0800

    add insert 100w new methods

--- Week_07/README.md
+++ Week_07/README.md
@@ -1,6 +1,6 @@
 #### 一、按自己设计的表结构，插入 100 万用户模拟数据，测试不同方式的插入效率
 
-一共测试四种方式：
+一共测试六种方式：
 
 ##### 1. PrepareStatement addBatch 开启自动提交事务
 ```
@@ -14,10 +14,19 @@ time: 195965ms = 196s = 3.26min
 ```
 time: 157209ms = 157s = 2.6min
 ```
-##### 4. PrepareStatement 将100w数据拼接为单条SQL执行
+##### 4. PrepareStatement 将100w数据拼接为 单 条SQL执行
 ```
 time: 19033ms = 19s
 ```
+##### 5.PrepareStatement 将100w数据拼接为 多 条SQL执行
+```
+time: 20s
+```
+##### 6.PrepareStatement addBatch 关闭自动提交事务，5000条/事务，jdbc-url添加：rewriteBatchedStatements=true
+```
+time: 13s
+```
+注：addBatch 默认是没有开启真正批处理的，只有开启rewriteBatchedStatements后，jdbc才会拼接sql一次打包请求
 
 
 #### 二、读写分离 - 动态切换数据源版本 1.0
--- Week_07/insert-data/src/main/java/jdbc/TestInsertData.java
+++ Week_07/insert-data/src/main/java/jdbc/TestInsertData.java
@@ -71,10 +71,6 @@ public class TestInsertData {
                 ps.setString(3,  String.valueOf(i));
                 ps.setString(4,  String.valueOf(i));
                 ps.addBatch();
-
-                if (i % 1000 == 0) {
-                    log.info("count：" + i);
-                }
             }
 
             ps.executeBatch();
@@ -92,10 +88,11 @@ public class TestInsertData {
     }
 
     // time: 157209ms = 157s = 2.6min
+    // 添加：rewriteBatchedStatements=true后，最低能达到13s
     @Test
     public void batchInsertMultiTx() {
         int batchCount = 1_000_000;
-        int txBatchCount = 5000;
+        int txBatchCount = 10000;
 
         Connection conn = null;
         PreparedStatement ps = null;
@@ -108,7 +105,7 @@ public class TestInsertData {
 
             long start = System.currentTimeMillis();
 
-            for(int i = 0; i < batchCount; i++) {
+            for(int i = 1; i <= batchCount; i++) {
                 ps.setInt(1, i);
                 ps.setString(2, "user_" + i);
                 ps.setString(3,  String.valueOf(i));
@@ -132,7 +129,7 @@ public class TestInsertData {
     }
 
 
-    // time: 19033ms = 19s
+    // 100w拼接为一条sql：time: 19033ms = 19s
     @Test
     public void batchInsertConcatValueString() {
         int batchCount = 1_000_000;
@@ -166,6 +163,54 @@ public class TestInsertData {
             ps.execute();
 
             conn.commit();
+
+            log.info("耗时：" + (System.currentTimeMillis() - start));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JdbcUtils.release(conn, ps,null);
+        }
+    }
+
+    // 100w拼接为多条sql：time: 20s
+    @Test
+    public void batchInsertConcatValueString2() {
+        int count = 1_000_000;
+        int batchCount = 50000;
+
+        Connection conn = null;
+        PreparedStatement ps = null;
+        try {
+            conn = JdbcUtils.getConnection();
+
+            conn.setAutoCommit(false);
+
+            String insertSql = "insert into user(user_id, user_name, mobile, password) values";
+
+            long start = System.currentTimeMillis();
+
+            StringBuilder stringBuilder = new StringBuilder(insertSql);
+
+            for(int i = 1; i <= count; i++) {
+                stringBuilder.append("(?, ?, ?, ?),");
+
+                if (i % batchCount == 0) {
+                    String sql = stringBuilder.toString();
+                    ps = conn.prepareStatement(sql.substring(0, sql.length() - 1));
+
+                    for (int j = 1; j <= batchCount; j++) {
+                        ps.setInt((j - 1) * 4 + 1, i);
+                        ps.setString((j - 1) * 4 + 2, "user_" + i);
+                        ps.setString((j - 1) * 4 + 3,  String.valueOf(i));
+                        ps.setString((j - 1) * 4 + 4,  String.valueOf(i));
+                    }
+                    ps.execute();
+                    conn.commit();
+
+                    stringBuilder = new StringBuilder(insertSql);
+                }
+            }
 
             log.info("耗时：" + (System.currentTimeMillis() - start));
 
